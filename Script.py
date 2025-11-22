fd735c080af8b9bf35268520a38ec690e214dd80
Movieroll bot's <gouravbolange@gmail.com>
2025-11-23 00:36:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -142,7 +142,7 @@ sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴍᴏᴠɪᴇ ᴏʀ sᴇʀɪᴇs ɴᴀᴍᴇ ᴀɴᴅ 
 ❗️ ʟᴀɴɢᴜᴀɢᴇ - {languages}</b>
 """
 
-    FILE_CAPTION = """<b><a href='https://t.me/Movie_Provider_group'>{file_name}</a></b>
+    FILE_CAPTION = """<b><a href='https://t.me/+0AGqflc6nBUxYjdl'>{file_name}</a></b>
 
 ⚜️ <b>ᴘᴏᴡᴇʀᴇᴅ ʙʏ:</b> <b><a href='https://t.me/Movieroll_M'>[ Mσʋιҽɾσʅʅ σϝϝιƈιαʅ ]</a></b>"""
 
