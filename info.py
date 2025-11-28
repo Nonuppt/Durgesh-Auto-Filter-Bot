79e8cc83d525173f98935511bcb1b1c39e80e998
Movieroll bot's <gouravbolange@gmail.com>
2025-11-28 10:03:39 +0530

    Remove default BOT_TOKEN value from info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_valid_ip(ip):
 
 API_ID = int(environ.get('API_ID', '21140176'))
 API_HASH = environ.get('API_HASH', 'b081ec8da8cf5263a6593041c1ae2a3b')
-BOT_TOKEN = environ.get('BOT_TOKEN', '8382484520:AAEW-BMyhlurr1uPuSSWQ7jICOkzNwLZdRE')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6222491731').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/Movieroll_M")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003281818923'))
